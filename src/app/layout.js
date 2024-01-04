a5cd8741b659bd4f9e75ae47ac2b10261e41afdd
unknown <mpvillamarin@gmail.com>
2024-01-04 16:10:09 -0500

    Nuestros Servicios

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
-import { Inter } from 'next/font/google'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { Playfair_Display } from 'next/font/google'
+ 
+const playfair = Playfair_Display({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+})
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={playfair.className}>
         <Navbar />
         {children}
         <Footer />
--- src/app/nuestros-servicios/page.jsx
+++ src/app/nuestros-servicios/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './nuestros-servicios.module.css';
+import Link from "next/link";
 
 export default function Servicios() {
 
@@ -18,8 +19,54 @@ export default function Servicios() {
                 </div>
             </div>
             <div className={styles.cont}>
-                <h2>Resistance te acompaña, te enseña y te da las herramientas para estar seguro</h2>
+                <h2 style={{
+                    fontSize: "35px",
+                    fontStyle: "italic", 
+                    fontWeight: "600"
+                }}
+                >Resistance te acompaña, te enseña y te da las herramientas para estar seguro</h2>
                 <p>Existen personas u organizaciones que buscan obtener tus datos ﬁnancieros, personales e incluso tu información privada. Podemos enseñarte a protegerte, tú decides cuando quieres aprender</p>
+                <div className={styles.contServices}>
+                    <div className={styles.contService}>
+                        <img
+                            src="images/Empresa.png"
+                            alt="Imagen Empresa"
+                            className={styles.image} />
+                        <h2>Empresas</h2>
+                        <p>Contamos con un amplio portafolio para fortalecer la seguridad de empresas de cualquier sector</p>
+                        <div className={styles.contLink}>
+                            <Link href="/nuestros-servicios/servicios-empresas" className={styles.buttonService}>
+                                Ver Más
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={styles.contService}>
+                        <img
+                            src="images/Familia.png"
+                            alt="Imagen Empresa"
+                            className={styles.image} />
+                        <h2>Familias</h2>
+                        <p>Acompañamos a ti y tu familia para protegerse de los riesgos en el ciber espacio</p>
+                        <div className={styles.contLink}>
+                            <Link href="/nuestros-servicios/servicios-familias-y-colegios" className={styles.buttonService}>
+                                Ver Más
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={styles.contService}>
+                        <img
+                            src="images/Escuelas.png"
+                            alt="Imagen Empresa"
+                            className={styles.image} />
+                        <h2>Colegios</h2>
+                        <p>Cursos interactivos para guiar a profesores y padres de familia en la implementación de controles tecnológicos en el ciber espacio</p>
+                        <div className={styles.contLink}>
+                            <Link href="/nuestros-servicios/servicios-familias-y-colegios" className={styles.buttonService}>
+                                Ver Más
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
