c768ff5e503a502455aa824eb4968b3ed56f356c
unknown <mpvillamarin@gmail.com>
2024-01-04 11:04:44 -0500

    ajustes

--- api/src/db.js
+++ api/src/db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  dialect: 'postgres', // Puedes cambiarlo según tu base de datos (por ejemplo, 'mysql', 'sqlite')
-  host: 'localhost', // Cambia esto según la configuración de tu base de datos
-  username: 'tu_usuario',
-  password: 'tu_contraseña',
-  database: 'tu_base_de_datos',
-  logging: false, // Puedes habilitar esto para ver las consultas SQL ejecutadas por Sequelize
+  dialect: 'postgres', 
+  host: 'localhost',
+  username: '',
+  password: '',
+  database: '',
+  logging: false, // Para ver las consultas SQL ejecutadas por Sequelize
 });
 
 // Prueba la conexión a la base de datos
@@ -21,11 +21,7 @@ async function conectarBD() {
 
 // Carga los modelos
 const models = {
-  Contacto: require('./models/contacto')(sequelize),
-  // Agrega otros modelos aquí si es necesario
 };
-
-// Asocia relaciones entre modelos si es necesario
 
 module.exports = {
   conectarBD,
--- src/app/nuestros-servicios/page.jsx
+++ src/app/nuestros-servicios/page.jsx
@@ -10,12 +10,16 @@ export default function Servicios() {
             </div>
             <div className={styles.contSec}>
                 <div className={styles.textHero}>
-                    <h3 className={styles.text}>Existen personas u organizaciones que buscan obtener tus datos ﬁnancieros, personales e incluso tu información privada. Podemos enseñarte a protegerte, tú decides cuando quieres aprender</h3>
+                    <h4 className={styles.text}>Existen personas u organizaciones que buscan obtener tus datos ﬁnancieros, personales e incluso tu información privada. Podemos enseñarte a protegerte, tú decides cuando quieres aprender</h4>
                 </div>
                 <div className={styles.contButtons}>
                     <button type="submit" className={styles.button}>Para Empresas</button>
                     <button type="submit" className={styles.button}>Para Familias</button>
                 </div>
+            </div>
+            <div className={styles.cont}>
+                <h2>Resistance te acompaña, te enseña y te da las herramientas para estar seguro</h2>
+                <p>Existen personas u organizaciones que buscan obtener tus datos ﬁnancieros, personales e incluso tu información privada. Podemos enseñarte a protegerte, tú decides cuando quieres aprender</p>
             </div>
         </div>
     )
